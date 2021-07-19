1d4e69dc3bac218b6052e801bf8a6bb9d4683018
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-07-19 14:32:29 -0700

    commit tree a0aa24dd748ab0507d5000aa765e26fa988912f1

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,10 @@
-# Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array_of_words)
+        sorted_word_chars = @word.chars.sort
+        array_of_words.select {|word| word.chars.sort == sorted_word_chars} || []
+    end
+end
